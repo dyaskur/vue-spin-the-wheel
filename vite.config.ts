341d18a4fddec1ddb346c2698dcf97b70a1296fe
️Yaskur Dyas⚔⚔️⚔ <dyas@yaskur.com>
2024-07-07 06:37:23 +0700

    chore: add vite-plugin-dts to generate .d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({
+    rollupTypes: true,
+    include: [
+      'src/components/WheelOfNames/index.vue',
+      'src/components/WheelOfNames/hooks/useRotate.ts',
+      'src/components/WheelOfNames/hooks/useCanvas.ts',
+      'src/components/WheelOfNames/types.ts',
+      'src/components/WheelOfNames/utils.ts',
+      'src/components/install.ts'],
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/components/install.ts'),
